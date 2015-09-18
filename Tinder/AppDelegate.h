73f988abeadb381c1e9d6a50f70b52824274b8d8
Devin Bhushan <devinb@yahoo-inc.com>
2015-09-18 12:35:39 -0700

    Initial Commit

---
+++ Tinder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tinder
+//
+//  Created by Devin Bhushan on 9/18/15.
+//  Copyright © 2015 Yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tinder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tinder
+//
+//  Created by Devin Bhushan on 9/18/15.
+//  Copyright © 2015 Yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
